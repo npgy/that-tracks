f7accb2b37ec1654c0da9e959f4b1054f304248d
npgy <me@np.gy>
2024-09-28 17:07:05 -0400

    ffmpeg stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,77 @@
 <script lang="ts">
-	import { FileDropzone, modeCurrent } from '@skeletonlabs/skeleton';
+	import { FileDropzone, ProgressBar, modeCurrent } from '@skeletonlabs/skeleton';
+	import { FFmpeg } from '@ffmpeg/ffmpeg';
+	import type { FileData, LogEvent } from '../../node_modules/@ffmpeg/ffmpeg/dist/esm/types';
+	import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
 	// Local
 	let files: FileList;
 
 	let imgUrl: string;
 
+	let vidOutput: FileData;
+
+	let encodeProgress: number = 0;
+	let encodeStarted: boolean = false;
+	let encodeDone: boolean = false;
+
+	const baseFfmpegURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm';
+
 	function onChangeHandler(e: Event): void {
 		console.log('file data:', e);
 		console.log('files:', files);
+
+		imgUrl = URL.createObjectURL(files[0]);
+	}
+
+	async function ffmpegStuff(): Promise<void> {
+		encodeStarted = true;
+		console.log('starting ffmpeg stuff');
+
+		const ffmpeg = new FFmpeg();
+		ffmpeg.on('log', ({ message: msg }: LogEvent) => {
+			console.log(msg);
+		});
+
+		ffmpeg.on('progress', (pe) => {
+			encodeProgress = pe.progress;
+			if (pe.progress === 1) {
+				encodeDone = true;
+			}
+
+			console.log(pe);
+		});
+
+		console.log('about to load');
+		await ffmpeg.load({
+			coreURL: await toBlobURL(`${baseFfmpegURL}/ffmpeg-core.js`, 'text/javascript'),
+			wasmURL: await toBlobURL(`${baseFfmpegURL}/ffmpeg-core.wasm`, 'application/wasm'),
+			workerURL: await toBlobURL(`${baseFfmpegURL}/ffmpeg-core.worker.js`, 'text/javascript')
+		});
+		console.log('loaded ffmpeg');
+
+		await ffmpeg.writeFile('test.mp4', await fetchFile(imgUrl));
+		console.log('writing input file');
+
+		await ffmpeg.exec(['-i', 'test.mp4', 'test.avi']);
+		console.log('encoding to avi');
+
+		vidOutput = await ffmpeg.readFile('test.avi');
 	}
 
 	function download(): void {
-		const fileUrl = URL.createObjectURL(files[0]);
+		const vidUrl = URL.createObjectURL(
+			new Blob([(vidOutput as Uint8Array).buffer], { type: 'video/avi' })
+		);
 
-		imgUrl = fileUrl;
 		let a = document.createElement('a');
 		document.body.appendChild(a);
 		a.setAttribute('style', 'display: none');
 
-		a.href = fileUrl;
-		a.download = 'blobby.jpg';
+		a.href = vidUrl;
+		a.download = 'test.avi';
 		a.click();
-		window.URL.revokeObjectURL(fileUrl);
+		window.URL.revokeObjectURL(vidUrl);
 	}
 
 	$: trackUnderLineClass = $modeCurrent ? 'decoration-cyan-700' : 'decoration-yellow-300';
@@ -49,7 +98,20 @@
 						<svelte:fragment slot="meta">MP3, WAV, JPG, PNG allowed.</svelte:fragment>
 					</FileDropzone>
 					<br />
-					<!-- <button class="btn variant-filled-primary" on:click={download}>Download</button> -->
+
+					<button class="btn variant-filled-primary mb-4" on:click={ffmpegStuff}
+						>Start Encode</button
+					>
+
+					{#if encodeStarted}
+						<ProgressBar value={encodeProgress} max={1} />
+					{/if}
+					<br />
+
+					{#if encodeDone}
+						<p class="mb-2">Your video is ready!</p>
+						<button class="btn variant-filled-primary" on:click={download}>Download</button>
+					{/if}
 					<!-- <img src={imgUrl} /> -->
 				</div>
 			</div>
