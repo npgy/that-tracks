40f2e06f576a1ab68024e4e74a55a2691ad02bb0
npgy <me@np.gy>
2024-09-29 11:30:57 -0400

    try optimize off

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 		}
 	},
 	optimizeDeps: {
-		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util', '@ffmpeg/core']
 	},
 	server: {
 		headers: {
