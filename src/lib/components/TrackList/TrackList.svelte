fe9908e477d432e76be4fe4b41cdca89c4d2aad3
npgy <me@np.gy>
2024-10-04 08:17:50 -0400

    some more styling

--- src/lib/components/TrackList/TrackList.svelte
+++ src/lib/components/TrackList/TrackList.svelte
@@ -15,11 +15,12 @@
 		<div class="flex flex-col">
 			{#each $filesStore as file, i}
 				<div
-					class="flex p-4 mb-2 bg-surface-100 dark:bg-surface-700 border-token rounded-token border-surface-200 dark:border-surface-600"
+					class="flex flex-row space-x-4 p-4 mb-2 bg-gray-100 dark:bg-surface-700 border-token rounded-token border-surface-200 dark:border-surface-600"
 					style="order: {i}}"
 				>
+					<i class="fa-solid fa-bars self-center hover:cursor-pointer"></i>
 					<span class="flex-auto">
-						<p class="text-xl">{file.name}</p>
+						<p class="text-md">{file.name}</p>
 					</span>
 				</div>
 			{/each}
